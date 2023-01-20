8ab71685669d9040168a4c57e17815c77ba7527b
rwmotivation <rwmotivation@gmail.com>
2023-01-20 12:16:57 +0530

    update traversal file

--- traverse.py
+++ traverse.py
@@ -2,3 +2,9 @@ k = input("enter a string")
 print("original string is",k)
 for ch in k:
     print(ch)
+
+#this is the copy of traversal 
+m = input("Please Enter your Date of Birth")
+print("Your Lucky Numbers for your car's plates are",m)
+for ch in m:
+    print(ch)
